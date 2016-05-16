6195b7f53385825f49c3977c47eb251b7e3ec8c6
edisonhello <edisonhello@hotmail.com>
2016-05-16 19:50:47 +0800

    FBapi

--- app.js
+++ app.js
@@ -84,17 +84,17 @@ app.get('/sylecss', function(req, res) {
 });
 
 io.on('connection',function(socket){
-    socket.on('reg q',function(name,birth,id,phone,email,school){
-        mongo.connect('mongodb://210.71.78.200:27017/summer2016',function(err,db){
+    socket.on('reg q',function(name,birth,id,phone,email,school,fbres){
+        mongo.connect('mongodb://db:27017/summer2016',function(err,db){
             if(err){
                 throw err;
             }
-            db.collection('reg').insert({name:name,birth:birth,id:id,phone:phone,email:email,school:school});
+            db.collection('reg').insert({name:name,birth:birth,id:id,phone:phone,email:email,school:school,fb:fbres});
             socket.emit('reg d');
         })
     })
     socket.on('que q',function(name,email,detail){
-        mongo.connect('mongodb://210.71.78.200:27017/summer2016',function(err,db){
+        mongo.connect('mongodb://db:27017/summer2016',function(err,db){
             if(err){
                 throw err;
             }
