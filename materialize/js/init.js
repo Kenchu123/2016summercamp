65ec174ac0973afb39e3d2438c2ff73eca56f5f5
Kenchu123 <k88913n@gmail.com>
2016-05-17 16:44:22 +0800

    regist_fixing2

--- materialize/js/init.js
+++ materialize/js/init.js
@@ -1,6 +1,12 @@
-  <script type="text/javascript">
-    $(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
-  });
-  
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+	$('select').material_select();
+	$('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 18, // Creates a dropdown of 15 years to control year
+    });
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
