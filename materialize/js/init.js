30f26e7324adb9382c2fbeb96906ffe1fdd3c238
Kenchu123 <k88913n@gmail.com>
2016-05-17 17:18:40 +0800

    fixing_regist6

--- materialize/js/init.js
+++ materialize/js/init.js
@@ -1,8 +1,6 @@
-$('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 32 
-    });
-
-    $(document).ready(function() {
-    $('select').material_select();
-});
+  <script type="text/javascript">
+    $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+  </script>
