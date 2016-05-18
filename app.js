45dffa18937aee8a9f7a000fe81598d2a15dd83c
edisonhello <edisonhello@hotmail.com>
2016-05-18 23:39:45 +0800

    add verify in app.js

--- app.js
+++ app.js
@@ -100,13 +100,27 @@ app.get('/sylecss', function(req, res) {
 
 io.on('connection',function(socket){
     socket.on('reg q',function(name,birth,id,phone,email,school,fbres,emername,emertel,eat,size,ps,date){
-        mongo.connect('mongodb://db:27017/summer2016',function(err,db){
-            if(err){
-                throw err;
+        if(!(name==""||birth==""||id==""||phone==""||email==""||school==""||emername=="" || emertel=="" || eat=="" || size=="")){
+            var idns = id.substring(1,id.length);
+            var idn = parseInt(idns);
+            var ida = id.substring(0,1);
+            var idan = parseInt(ida);
+            if(isNaN(idan) && idn>100000000 && idn<299999999){
+                mongo.connect('mongodb://db:27017/summer2016',function(err,db){
+                    if(err){
+                        throw err;
+                    }
+                    db.collection('reg').insert({name:name,birth:birth,id:id,phone:phone,email:email,school:school,fb:fbres,emername:emername,emertel:emertel,eat:eat,size:size,ps:ps,date:date});
+                    socket.emit('reg d');
+                }
+            })
+            else{
+                socket.emit("reg f");
             }
-            db.collection('reg').insert({name:name,birth:birth,id:id,phone:phone,email:email,school:school,fb:fbres,emername:emername,emertel:emertel,eat:eat,size:size,ps:ps,date:date});
-            socket.emit('reg d');
-        })
+        }
+        else{
+            socket.emit("reg f");
+        }
     })
     socket.on('que q',function(name,email,detail){
         mongo.connect('mongodb://db:27017/summer2016',function(err,db){
