ebb9c0a2087f6b46f391a76bbd839c4f3c5c7acd
Kenchu123 <k88913n@gmail.com>
2016-05-18 14:06:00 +0800

    datepicker-fix

--- materialize/js/ss.js
+++ materialize/js/ss.js
@@ -1,6 +1,8 @@
   $('.datepicker').pickadate({
         selectMonths: true, // Creates a dropdown to control month
-        selectYears: 32 
+        selectYears: 6,
+		min: new Date(2000,1,1),
+   		max: new Date(2003,12,31)
     });
 
     $(document).ready(function() {
